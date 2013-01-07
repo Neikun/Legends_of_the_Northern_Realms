9cf4c940fa4547dddd081a8d7cf841823fedd56e
Centauri Soldier <centaurisoldier@gmail.com>
2013-01-06 17:50:28 -0800

    Added some more items to the script info file and moved some folders and files

--- Script Requests/To Level Designers.lua
+++ Script Requests/To Level Designers.lua
@@ -2,8 +2,9 @@
 be updated regularly and should be considered the main info document for all
 scripting done by the scripting team. Should you have scripting ideas or requests,
 please post them in a text file within the 'Script Requests' folder inside
-this folder using your name as the filename. Should you have a
-question that is not answered here, please feel free to contact Centauri Soldier or any other of the scripters detailed in the Official Thread.>>
+this folder using your name as the filename. Should you have a question that
+is not answered here, please feel free to contact Centauri Soldier or any other
+of the scripters detailed in the Official Thread.>>
 
 [Overview]
 This is a scripting reference intended for the level designers.
@@ -14,6 +15,28 @@ method(s) mentioned above.
 This contains general instructions on how to implement
 scripts during level design and detailed info on those
 scripts.
+
+[DSRS]*
+Data Storage and Retrieval System. This is the system that
+allows the saving and loading of data that is superficial.
+Any data that the game does not save by default, like party
+gold, NPC behavior and so on.
+
+[Modules List]
+	
+	{Complete}
+		
+	{Working}
+	GUI - Just like it sounds, this modules handles things like dialogs, shop interfaces and so on.
+	Game - Contains the methods used for accessing the (DSRS)* as well as any other global elements of the game.
+	Help - Accessible in-game, this module will allow players to learn about new additions like shops, the game economy and NPCs.
+	Monsters - Used for the random generation of monsters in the dungeon.
+	NPC - Handles all thigs concerning party/NPC interaction as well as NPC creation, editing and deletion.
+	Party - keeps track of superficial things like gold, relationships with NPCs etc.: things that are not saved by the game but only by the custom (DSRS)*.
+	Util - Contains Math, Table and many other utilty functions. Meant as a support module for the others.
+		
+	{Planned}
+
 
 [Basic Information Regarding Script Module Layout]
 All methods will have a header with the following information.
