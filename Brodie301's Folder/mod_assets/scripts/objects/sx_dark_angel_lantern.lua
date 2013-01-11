186ff8c03103e783afe74a153c07245b53dd06df
jason <brodie301@hotmail.com>
2013-01-10 22:01:18 -0600

    Added new folders

---
+++ Brodie301's Folder/mod_assets/scripts/objects/sx_dark_angel_lantern.lua
@@ -0,0 +1,8 @@
+defineObject{
+	name = "dark_angel_lantern",
+	class = "Blockage",
+	model = "mod_assets/models/sx_dark_angel_lantern.fbx",
+	placement = "wall",
+	replacesWall = true,
+	editorIcon = 92,
+}
