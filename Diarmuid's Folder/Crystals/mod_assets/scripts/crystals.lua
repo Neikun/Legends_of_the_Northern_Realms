12e18143deb2c756a327b473898e5e2fcaebe785
Georges Dimitrov <georges.dimitrov@gmail.com>
2013-02-16 14:37:41 -0500

    Updated Crystals WIP

--- Diarmuid's Folder/Crystals/mod_assets/scripts/crystals.lua
+++ Diarmuid's Folder/Crystals/mod_assets/scripts/crystals.lua
@@ -1,77 +1,18 @@
-defineMaterial{
-	name = "healing_crystal_red",
-	diffuseMap = "mod_assets/textures/nightfall_soul_red.tga",
-	specularMap = "assets/textures/env/healing_crystal_spec.tga",
-	normalMap = "assets/textures/env/healing_crystal_normal.tga",
-	doubleSided = false,
-	lighting = true,
-	alphaTest = false,
-	blendMode = "Opaque",
-	textureAddressMode = "Wrap",
-	glossiness = 20,
-	depthBias = 0,
-}
+-- ***********************************************************************************
+--                        Define main crystal object
+-- ***********************************************************************************
 
-defineMaterial{
+cloneObject{
 	name = "healing_crystal_green",
-	diffuseMap = "mod_assets/textures/nightfall_soul_green.tga",
-	specularMap = "assets/textures/env/healing_crystal_spec.tga",
-	normalMap = "assets/textures/env/healing_crystal_normal.tga",
-	doubleSided = false,
-	lighting = true,
-	alphaTest = false,
-	blendMode = "Opaque",
-	textureAddressMode = "Wrap",
-	glossiness = 50,
-	depthBias = 0,
-}
-
-defineMaterial{
-	name = "healing_crystal_green_additive",
-	diffuseMap = "mod_assets/textures/nightfall_soul_green_additive.tga",
-	specularMap = "assets/textures/env/healing_crystal_spec.tga",
-	normalMap = "assets/textures/env/healing_crystal_normal.tga",
-	doubleSided = false,
-	lighting = true,
-	alphaTest = true,
-	blendMode = "Additive",
-	textureAddressMode = "Wrap",
-	glossiness = 20,
-	depthBias = 0,
-}
-
-defineMaterial{
-	name = "sx_invisible",
-	diffuseMap = "assets/textures/common/black.tga",
-	doubleSided = false,
-	lighting = true,
-	alphaTest = true,
-	blendMode = "Additive",
-	textureAddressMode = "Wrap",
-	glossiness = 5,
-	depthBias = 0,
-}
-
-defineObject{
-	name = "healing_crystal_red",
-	class = "Crystal",
-	model = "mod_assets/models/healing_crystal_red.fbx",
-	animation = "assets/animations/env/healing_crystal_spin.fbx",
-	particleSystem = "crystal_red",
-	placement = "floor",
+	baseObject = "script_entity",
 	editorIcon = 60,
 }
 
-defineObject{
-	name = "healing_crystal_green_crystal",
-	class = "Crystal",
-	model = "mod_assets/models/healing_crystal_green.fbx",
-	animation = "assets/animations/env/healing_crystal_spin.fbx",
-	particleSystem = "crystal_green",
-	placement = "floor",
-	editorIcon = 60,
-}
+-- ***********************************************************************************
+--                     Define crystal projectile objects
+-- ***********************************************************************************
 
+-- Main crystal, activated
 defineObject{
 	name = "_healing_crystal_green_main",
 	class = "Item",
@@ -84,18 +25,7 @@ defineObject{
 	fragile = true,
 }
 
-defineObject{
-	name = "_healing_crystal_green_small",
-	class = "Item",
-	model = "mod_assets/models/healing_crystal_green_small.fbx",
-	gfxIndex = 1,
-	uiName = "_healing_crystal_green_main",
-	weight = 0.1,
-	projectileRotationSpeed = -0.6,
-	projectileRotationY = 1,
-	fragile = true,
-}
-
+-- Main crystal, activated - shader additive version
 defineObject{
 	name = "_healing_crystal_green_main_additive",
 	class = "Item",
@@ -108,6 +38,50 @@ defineObject{
 	fragile = true,
 }
 
+-- Main crystal, deactivated
+defineObject{
+	name = "_healing_crystal_green_main_deactivated",
+	class = "Item",
+	model = "mod_assets/models/healing_crystal_green_main.fbx",
+	gfxIndex = 1,
+	uiName = "_healing_crystal_green_main",
+	weight = 0.1,
+	projectileRotationSpeed = 0.1,
+	projectileRotationY = 1,
+	fragile = true,
+}
+
+-- Small crystals, activated
+defineObject{
+	name = "_healing_crystal_green_small",
+	class = "Item",
+	model = "mod_assets/models/healing_crystal_green_small.fbx",
+	gfxIndex = 1,
+	uiName = "_healing_crystal_green_main",
+	weight = 0.1,
+	projectileRotationSpeed = -0.6,
+	projectileRotationY = 1,
+	fragile = true,
+}
+
+-- Small crystals, deactivated
+defineObject{
+	name = "_healing_crystal_green_small_deactivated",
+	class = "Item",
+	model = "mod_assets/models/healing_crystal_green_small.fbx",
+	gfxIndex = 1,
+	uiName = "_healing_crystal_green_main",
+	weight = 0.1,
+	projectileRotationSpeed = -0.15,
+	projectileRotationY = 1,
+	fragile = true,
+}
+
+-- ***********************************************************************************
+--                           Define crystal lightsources
+-- ***********************************************************************************
+
+-- Activated state lightsources
 defineObject{
 	name = "_healing_crystal_green_light",
 	class = "LightSource",
@@ -121,6 +95,20 @@ defineObject{
 	editorIcon = 88,
 }
 
+-- Dectivated state lightsources
+defineObject{
+	name = "_healing_crystal_green_light_deactivated",
+	class = "LightSource",
+	lightPosition = vec(1, 1.5, 1),
+	lightRange = 5,
+	lightColor = vec(0.5, 1, 0.5),
+	brightness = 1,
+	castShadow = false,
+	flicker = false,
+	placement = "floor",
+	editorIcon = 88,
+}
+-- Particle system lightsource
 defineObject{
 	name = "_healing_crystal_green_particleSystem",
 	class = "LightSource",
@@ -134,6 +122,10 @@ defineObject{
 	placement = "floor",
 	editorIcon = 88,
 }
+
+-- ***********************************************************************************
+--                       Define crystal helper objects
+-- ***********************************************************************************
 
 defineObject{
 	name = "_healing_crystal_altar",
@@ -151,6 +143,10 @@ cloneObject{
 	baseObject = "heavy_shield",
 	model =  "mod_assets/models/healing_crystal_fake_item.fbx",	
 }
+
+-- ***********************************************************************************
+--                      Define crystal particleSystems
+-- ***********************************************************************************
 
 defineParticleSystem{
 	name = "crystal_green",
@@ -203,59 +199,47 @@ defineParticleSystem{
 	}
 }
 
-defineParticleSystem{
-	name = "crystal_red",
-	emitters = {
-		-- fog
-		{
-			emissionRate = 10,
-			emissionTime = 0,
-			maxParticles = 1000,
-			boxMin = {-0.5, 0.0,-0.5},
-			boxMax = { 0.5, 2.5, 0.5},
-			sprayAngle = {0,360},
-			velocity = {0.1,0.2},
-			objectSpace = true,
-			texture = "assets/textures/particles/fog.tga",
-			lifetime = {3,3},
-			color0 = {2.0, 0.4, 0.4},
-			opacity = 1,
-			fadeIn = 2.2,
-			fadeOut = 2.2,
-			size = {1.5, 1.5},
-			gravity = {0,0,0},
-			airResistance = 0.1,
-			rotationSpeed = 0.3,
-			blendMode = "Additive",
-		},
+-- ***********************************************************************************
+--                          Define crystal materials
+-- ***********************************************************************************
 
-		-- stars
-		{
-			emissionRate = 100,
-			emissionTime = 0,
-			maxParticles = 200,
-			boxMin = {-0.6, 0.3,-0.6},
-			boxMax = { 0.6, 2.5, 0.6},
-			sprayAngle = {0,360},
-			velocity = {0.02,0.05},
-			objectSpace = true,
-			texture = "assets/textures/particles/teleporter.tga",
-			lifetime = {1,2},
-			color0 = {4.0,1.0,1.0},
-			opacity = 0.8,
-			fadeIn = 0.1,
-			fadeOut = 3,
-			size = {0.04, 0.1},
-			gravity = {0,0,0},
-			airResistance = 1,
-			rotationSpeed = 1,
-			blendMode = "Additive",
-		}
-	}
-}
-
-cloneObject{
+defineMaterial{
 	name = "healing_crystal_green",
-	baseObject = "script_entity",
-	editorIcon = 60,
+	diffuseMap = "mod_assets/textures/nightfall_soul_green.tga",
+	specularMap = "assets/textures/env/healing_crystal_spec.tga",
+	normalMap = "assets/textures/env/healing_crystal_normal.tga",
+	doubleSided = false,
+	lighting = true,
+	alphaTest = false,
+	blendMode = "Opaque",
+	textureAddressMode = "Wrap",
+	glossiness = 50,
+	depthBias = 0,
 }
+
+defineMaterial{
+	name = "healing_crystal_green_additive",
+	diffuseMap = "mod_assets/textures/nightfall_soul_green_additive.tga",
+	specularMap = "assets/textures/env/healing_crystal_spec.tga",
+	normalMap = "assets/textures/env/healing_crystal_normal.tga",
+	doubleSided = false,
+	lighting = true,
+	alphaTest = true,
+	blendMode = "Additive",
+	textureAddressMode = "Wrap",
+	glossiness = 20,
+	depthBias = 0,
+}
+
+defineMaterial{
+	name = "sx_invisible",
+	diffuseMap = "assets/textures/common/black.tga",
+	doubleSided = false,
+	lighting = true,
+	alphaTest = true,
+	blendMode = "Additive",
+	textureAddressMode = "Wrap",
+	glossiness = 5,
+	depthBias = 0,
+}
+
