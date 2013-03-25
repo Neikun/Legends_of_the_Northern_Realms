0f555f34ab556e22e9918ff17782562072eb0117
msyblade <msyblade@yahoo.com>
2013-03-25 14:54:36 -0600

    removed blubber

--- msyblade's folder/msy_Town_Test/mod_assets/sx_towntileset/scripts/sx_towntileset_sounds.lua
+++
@@ -1,9 +0,0 @@
-defineSound{
-       name = "sx_stream",
-       filename = "mod_assets/sx_towntileset/sounds/sx_stream.wav",
-       loop = true,
-       volume = 1,
-       minDistance = 1,
-       maxDistance = 3,
-    }
-	
